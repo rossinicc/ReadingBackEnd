e9e46c477f13e62e202bbf73cf9582ce50cb7919
rossinicc <rossini.vilela.caroca@ccc.ufcg.edu.br>
2025-09-11 17:51:55 -0300

    feat: inicializando projeto quarkus

---
+++ app-reading/src/test/java/org/reading/GreetingResourceIT.java
@@ -0,0 +1,8 @@
+package org.reading;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+class GreetingResourceIT extends GreetingResourceTest {
+    // Execute the same tests but in packaged mode.
+}
